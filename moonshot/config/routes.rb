21afd761f00a48bf4f729cfdca298f2ebf9751bc
Adel Mandanas <mandanas.adel@gmail.com>
2018-03-24 01:28:09 +0800

    Add routes

--- moonshot/config/routes.rb
+++ moonshot/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :cards
+  get '/cards/sid/:sid/' => 'cards#get_sid'
+  post '/cards/pump/' => 'cards#post_sid'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
