9dab78781b998969e8374dff07e5684068065e30
DEGABGED <franciszac.dlc@gmail.com>
2018-03-24 10:53:48 +0800

    Remove test files

--- http-test.rb
+++
@@ -1,8 +0,0 @@
-require 'net/http'
-
-url = URI.parse('http://localhost:3000')
-req = Net::HTTP::Get.new(url.to_s)
-res = Net::HTTP.start(url.host, url.port) {|http|
-    http.request(req)
-}
-puts res.body
